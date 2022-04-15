8cfd7e8f5b6bb439dd874e2724b3ad0070a79d2c
GeorgeSemenov <zhora.sem@yandex.ru>
2022-04-15 11:23:50 +0300

    build work well

--- webpack/images.js
+++ webpack/images.js
@@ -25,23 +25,39 @@ module.exports = function(){
 	              ["jpegtran", { progressive: true }],
 	              ["optipng", { optimizationLevel: 5 }],
 	              // Svgo configuration here https://github.com/svg/svgo#configuration
-	              [
-	                "svgo",
-	                {
-	                  plugins: extendDefaultPlugins([
-	                    {
-	                      name: "removeViewBox",
-	                      active: false,
-	                    },
-	                    {
-	                      name: "addAttributesToSVGElement",
-	                      params: {
-	                        attributes: [{ xmlns: "http://www.w3.org/2000/svg" }],
-	                      },
-	                    },
-	                  ]),
-	                },
-	              ],
+	              // [
+	              //   "svgo",
+	              //   {
+	              //   	plugins: extendDefaultPlugins([
+	              //       {
+	              //         name: "preset-default",
+	              //         params: {
+									      //   overrides: {
+									      //     // customize default plugin options
+									      //     inlineStyles: {
+									      //       onlyMatchedOnce: false,
+									      //     },
+
+									      //     // or disable plugins
+									      //     removeDoctype: false,
+									      //   },
+									      // },
+	              //       },
+	              //     ]),
+	              //     // plugins: extendDefaultPlugins([
+	              //     //   {
+	              //     //     name: "removeViewBox",
+	              //     //     active: false,
+	              //     //   },
+	              //     //   {
+	              //     //     name: "addAttributesToSVGElement",
+	              //     //     params: {
+	              //     //       attributes: [{ xmlns: "http://www.w3.org/2000/svg" }],
+	              //     //     },
+	              //     //   },
+	              //     // ]),
+	              //   },
+	              // ],
 	            ],
 	          },
 	        },
