c9236acf601401c63214c29f91d321f7a1f62fd9
GeorgeSemenov <zhora.sem@yandex.ru>
2022-04-18 15:59:07 +0300

    it's new begining

--- source/pages/index/index.js
+++ source/pages/index/index.js
@@ -1,6 +1,4 @@
 import './index.scss';
 import '../../JS-components/libs.js';
 import '../../blocks/form-and-elements/form-and-elements.js';
-import '../../blocks/cards/cards.js';
 
-console.log("чё пыришься?");
